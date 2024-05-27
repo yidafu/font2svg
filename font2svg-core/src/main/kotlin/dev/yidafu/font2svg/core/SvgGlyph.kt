c861b5ddd0bfa6d9df8587a6151502aad38497cc
yidafu <me@yidefu.dev>
2024-05-28 00:09:32 +0800

    refactor(core): generate SvgGlyph instead of svg string

--- font2svg-core/src/main/kotlin/dev/yidafu/font2svg/core/SvgGlyph.kt
+++ font2svg-core/src/main/kotlin/dev/yidafu/font2svg/core/SvgGlyph.kt
@@ -1,4 +1,9 @@
 package dev.yidafu.font2svg.dev.yidafu.font2svg.core
 
-class Glyph {
+data class SvgGlyph(
+    val viewBox: String,
+    val path: String,
+    val ascender: Int,
+    val descender: Int,
+) {
 }
